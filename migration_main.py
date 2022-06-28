421b0775a1de6cce19155d61a2ef4328cd50f353
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-06-29 00:00:46 +0900

    Update migration_main.py

--- migration_main.py
+++ migration_main.py
@@ -1,4 +1,6 @@
 import sys
+import math
+import time
 
 # file_lo = input('파일의 경로를 입력하세요 : ')
 # file_name = input('파일의 이름을 입력하세요 : ')
@@ -11,18 +13,21 @@ forti_grp = []
 forti_policy = []
 forti_route = []
 line = None
-file = open('C:\\Users\\유형석\\Desktop\\hey.txt', mode='r')
 
-# file_read = file.readline()
-while line != '':
-    line = file.readline()
-    if 'network-object' in line:
-        print(line, end='')
-        # forti_addr.append()
-    elif 'object-group network' in line:
-        pass
-        # forti_grp.append()
+start_time = time.time()
+file = open('C:\\Users\\K1220006\\Desktop\\미디어윌 ASA 5505.txt', mode='r')
+file_data = file.read().splitlines()
 
-file.close()
+forti_addr = [x for x in file_data if 'network-object' in x]
+end_time = time.time()
 
-print(type(line))
+print(forti_addr)
+print(f"{end_time - start_time: .5f} sec")
+
+# while line != '':
+#     line = file.readline()
+#     if 'network-object' in line:
+#         print(line)
+#     elif 'object-group network' in line:
+#         pass
+#
