2075a914290ed9e15a596cefd46b0e4fef713084
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-07-05 17:45:12 +0900

    Update migration_main.py

--- migration_main.py
+++ migration_main.py
@@ -6,7 +6,7 @@ import time
 # file_name = input('파일의 이름을 입력하세요 : ')
 
 # file_rlo = file_lo + file_name
-
+# https://velog.io/@learningssik/Github-%EC%8B%A0%EC%83%81-%EB%B1%83%EC%A7%80-5%EB%B6%84%EB%A7%8C%EC%97%90-%EC%96%BB%EB%8A%94-%EB%B0%A9%EB%B2%95
 # file = open('{0}{1}'.format(file_lo, file_name), mode='r')
 forti_addr = []
 forti_grp = []
