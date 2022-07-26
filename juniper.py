4903f6bda78db8f977f6605bbcb8a3703fc56a23
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-07-26 18:22:44 +0900

    Update juniper.py

--- juniper.py
+++ juniper.py
@@ -1,3 +1,21 @@
+import re
+
 file_name = input("file name : ")
 
 print(file_name)
+
+class juniper:
+    def __init__(self, data):
+        self.data = data
+    def network(self, data):
+        re_addr = re.compile("ipaddr")
+        data_addr = re_addr.search(data)
+        return data_addr
+
+    def service(self, data):
+        re_service = re.compile("service")
+        data_service = re_service.search(data)
+        return data_service
+
+    def manageip(self, data):
+        re_manageip = re.compile("")
