338b89117e1f6a7061fd11153f382af301d7b995
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-08-06 01:40:11 +0900

    Update juniper.py

--- juniper.py
+++ juniper.py
@@ -1,21 +1,32 @@
 import re
-
-file_name = input("file name : ")
-
-print(file_name)
+from typing import List
 
 class juniper:
-    def __init__(self, data):
-        self.data = data
-    def network(self, data):
-        re_addr = re.compile("ipaddr")
-        data_addr = re_addr.search(data)
-        return data_addr
+    def __init__(self):
+        pass
 
-    def service(self, data):
+    def trans_addr(self, line, new_line) -> List:
+        fgt_addr = [i.split() for i in line if "set address" in i]
+        new_line.write('config firewall address\n')
+
+        for i in fgt_addr:
+            print('config firewall address', '\n','edit name {0}'.format(i[3]), '\n\t',
+                  'set subnet {0} {1}'.format(i[4], i[5]), '\n', 'next')
+            new_line.write('edit ')
+            new_line.write(i[3])
+            new_line.write('\n')
+            new_line.write('set subnet ')
+            new_line.write(i[4])
+            new_line.write(' ')
+            new_line.write(i[5])
+            new_line.write('\n')
+            new_line.write('next\n')
+        new_line.write('\n')
+
+    def trans_service(self, line) -> List:
         re_service = re.compile("service")
-        data_service = re_service.search(data)
+        data_service = re_service.search(line)
         return data_service
 
-    def manageip(self, data):
+    def trans_manageip(self, line) -> List:
         re_manageip = re.compile("")
