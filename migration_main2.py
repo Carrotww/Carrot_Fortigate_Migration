455cf86a24ee69479ff9a456c3441880921cb852
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-08-19 14:11:20 +0900

    Delete migration_main2.py

--- migration_main2.py
+++
@@ -1,9 +0,0 @@
-import sys
-import time
-
-class ASA_migration:
-    def __init__(self, forti_addr, forti_grp):
-        self.forti_addr = forti_addr
-        self.forti_grp = forti_grp
-    def asa_readfile(self, file):
-        pass
